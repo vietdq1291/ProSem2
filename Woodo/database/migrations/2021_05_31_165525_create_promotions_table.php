49b2bffbe9bdf8aeb41559ca97e1c54f56c7ce02
Haivinh0704 <you@example.com>
2021-06-01 00:32:18 +0700

    Vinh 1/6 00:32
    Vinh Create Project + laravel/ui + Models

---
+++ Woodo/database/migrations/2021_05_31_165525_create_promotions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePromotionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->id();
+            $table->char('pro_name',225);
+            $table->char('pro_description',225);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promotions');
+    }
+}
